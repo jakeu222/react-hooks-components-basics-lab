e0b15be7a9633a6ddf4bcb27d24327c1c479f9b3
Jake Underwood <jakeunderwood2211@yahoo.com>
2023-08-21 20:20:35 -0600

    complete lab

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,28 @@
 import React from "react";
+import About from "./About";
+import Home from "./Home";
+import NavBar from "./Navbar";
 
-function NavBar() {
-  return (
-    <nav>
-      <a href="#home">I'm a link!</a>
-    </nav>
-  );
-}
 
-function Home() {
-  return (
-    <div id="home">
-      <h1>Home</h1>
-    </div>
-  );
-}
 
-{/* write an <About> component here */}
+
+// function About() {
+//   return (
+//     <div id="About">
+//       <h1>About</h1>
+
+//     </div>
+//   )
+// }
+// // export default About
 
 
 function App() {
   return (
     <div>
       <NavBar />
-      {/* add the <Home> component here */}
-      {/* add your <About> component here */}
+      <Home />
+      <About />
     </div>
   );
 }
